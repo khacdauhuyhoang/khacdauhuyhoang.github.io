ba039683764c7e7688ff0896adf156e2a28509e2
khacdauhuyhoang <khacdauhuyhoang.lxag@gmail.com>
2026-01-15 17:07:49 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ZaloButton from "./components/ZaloButton";
+
 import Home from "./routes/Home";
 import Pricing from "./routes/Pricing";
 import Contact from "./routes/Contact";
-import ZaloButton from "./components/ZaloButton";
+import LocationPage from "./routes/LocationPage";
+import ServicePage from "./routes/ServicePage";
 
 export default function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/bang-gia" element={<Pricing />} />
         <Route path="/lien-he" element={<Contact />} />
+
+        {/* SEO khu vực */}
+        <Route path="/khu-vuc/:slug" element={<LocationPage />} />
+
+        {/* Trang dịch vụ đơn giản (dùng để đi link từ Home) */}
+        <Route path="/dich-vu/:slug" element={<ServicePage />} />
       </Routes>
       <ZaloButton />
+      <Footer />
     </>
   );
 }
